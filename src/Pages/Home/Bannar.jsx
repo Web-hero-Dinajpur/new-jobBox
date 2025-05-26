380e4a8d71f9b3948e0d34f1cffecd75677a914c
Sreedhar-roy <sreedharroy0@gmail.com>
2025-05-26 11:28:25 +0600

    bannar and bannar top text section commit

--- src/Pages/Home/Bannar.jsx
+++ src/Pages/Home/Bannar.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import * as motion from "motion/react-client"
 import { easeOut } from 'motion';
+import team1 from '../../assets/team/team1.jpg'
+import team2 from '../../assets/team/team2.jpg'
 
 const Bannar = () => {
     return (
         <div className="hero bg-base-200 min-h-96">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className='flex-1'>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                        className="max-w-sm rounded-lg shadow-2xl"
+                    <motion.img
+                        animate={{ y: [50, 100, 50] }}
+                        transition={{ duration: 10, delay: 1, ease: easeOut, repeat: Infinity }}
+                        src={team1}
+                        className="max-w-sm w-64 rounded-br-[30px] rounded-t-[30px] border-l-8 border-b-8 border-blue-500 shadow-2xl"
+                    />
+                    <motion.img
+                        animate={{ x: [50, 150, 50] }}
+                        transition={{ duration: 10, delay: 1, ease: easeOut, repeat: Infinity }}
+                        src={team2}
+                        className="max-w-sm w-64 rounded-br-[30px] rounded-t-[30px] border-l-8 border-b-8 border-blue-500 shadow-2xl"
                     />
                 </div>
                 <div className='flex-1'>
-                    <h1 className="text-5xl font-bold">Latest job for you!</h1>
-                    <motion.h1 
-                    animate={{x: 60}} 
-                    transition={{duration: 2, delay: 1, ease: easeOut, repeat:Infinity,}}
-                    className="text-5xl font-bold">Latest job for you!</motion.h1>
+                    <motion.h1
+                        animate={{ x: 60 }}
+                        transition={{ duration: 2, delay: 1, ease: easeOut, repeat: Infinity, }}
+                        className="text-5xl font-bold">Latest <motion.span
+                            animate={{ color: ['#41EAE2', '#41ACEA', '#4155EA'] }}
+                            transition={{ duration: 1.5, repeat: Infinity }}
+                        >Jobs </motion.span>for you!</motion.h1>
                     <p className="py-6">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
